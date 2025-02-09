8e785d086faf84633423f40f06714200928868df
Ashl3yyy07 <ashleyhk64@gmail.com>
2025-02-09 17:14:16 -0600

    Add files via upload

--- java.js
+++ java.js
@@ -4,3 +4,29 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky", this.window.scrollY > 0);
 })
+
+
+//MENU BAR
+
+let menuBar = document.querySelector('#menu-bar');
+let menuPage = document.querySelector('#menu-page');
+let html = document.querySelector('html');
+
+
+
+
+
+//Show/hide menu bar
+
+menuBar.addEventListener('click', () => {
+    menuPage.classList.toggle('active');
+
+    html.style.overflow = (menuPage.classList.contains('active')) ? "hidden" : "scroll"
+} )
+
+
+function accessCounter(spanElement){
+    if (screenType === 'desktop') {
+        return document.querySelector('#nav')
+    }
+}
